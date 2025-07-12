57312d5c31ea559a063bb0a954fd3a8bdc398c7c
elbriant <elbrianl14@gmail.com>
2025-07-12 15:39:10 -0400

    fix vitepress base error?

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vitepress";
 export default defineConfig({
   title: "Sagapi",
   description: "An arknights api for game resources",
-  base: "/docs",
+  base: "/",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
